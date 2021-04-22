6d48c2276a433337998f269cf31c89ac5f82699b
Delsin <devagr@hotmail.com>
2021-04-22 20:17:39 +0200

    Skelet for action

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios").default;
+
+try {
+    const siteUrl = core.getInput("site");
+    const apiKey = core.getInput("key");
+    const apiUrl = 'https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run?key=' + apiKey;
+
+    axios.post(apiUrl, {
+        url: siteUrl,
+        requestScreenshot: true
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
